e35401af2133b092363459fd9316e353ae6ac97d
KityLinn <87649871+KityLinn@users.noreply.github.com>
2026-02-08 12:00:20 +0100

    groundwork

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import { useState } from 'react'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
+    <h1 className=" p-3">TEST</h1>
 
     </>
   )
